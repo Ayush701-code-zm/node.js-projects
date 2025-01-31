54f5bc796471f0e5118855454661f525b9b304aa
Ayush701-code-zm <h3yayush@gmail.com>
2025-01-31 22:27:33 +0530

    xcvhh

---
+++ 3-http-mod/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req, "req");
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("hii");
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
